76a134f3ba41b8bc9c4ab13cae331f15fe2560a3
Blueberryple <69690333+Blueberryple@users.noreply.github.com>
2020-08-14 10:35:27 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is HaoMing Yu but being called Hao is fine.");
+		System.out.println("I enjoy cooking, reading, biking, gardening, and playing games.");
+		System.out.println("I am a man of convenience.");
+		System.out.println("I consider majoring in the computer science field in the future.");
+		System.out.println("I like to play gacha games");
+	}
+	
+}
